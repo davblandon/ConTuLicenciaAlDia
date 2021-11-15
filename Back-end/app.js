93bc95c2d39bf6dc3d82e6763d59f6425bb1d41a
Kevin Caicedo <keviinvallenato2002@gmail.com>
2021-11-15 17:53:36 -0500

    cambios

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,23 +1,55 @@
-<<<<<<< HEAD
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
 const express= require('express');
 var app = express();
 const mongoose = require('mongoose');
 var cookieParser = require('cookie-parser')
-=======
 const express= require('express')
 const passport = require('passport');//login
 const cookieParser = require('cookie-parser');//login
 const session = require('express-session');
 const passportLocal = require('passport').Strategy;//login
->>>>>>> e49670600239920f6f8a54086546de9e76df57ae
-
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-// const bcrypt = require('bcrypt');
 
 
+
+//MIDDELWARE
+app.use(morgan('tiny'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Para acceder al directorio actual
+
+
+// app.get('/', function(req,res){
+//     res.send('Hello World!');
+// });
+
+app.use('/api', require('./routes/users'));
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// ASIGNAR PUERTO ESPECIFICO
+// app.listen(3000,function name() {
+//     console.log('Example app listing on port 3000!');
+// })
+
+// ASIGNAR PUERTO DINAMICO
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), function () {
+ console.log('Contectado al puerto '+ app.get('puerto'));
+});
+
+
+
+
 
 
 // Configurar cabeceras y cors
@@ -33,77 +65,3 @@ app.use(function (req, res, next) {
   });
 app.use(require('./routers/router'));
 module.exports = app;
-
-
-
-//leer datos de un formulario
-// app.use(express.urlencoded({extended: true}));
-
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
-passport.use(new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, res.send ({ message: 'Incorrect password.' }));
-      }
-      return done(null, user);
-    });
-  }
-));
-
-
-var session = require("express-session"),
-    bodyParser = require("body-parser");
-
-app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-<<<<<<< HEAD
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-=======
-app.use(passport.initialize());
-app.use(passport.session());
->>>>>>> e49670600239920f6f8a54086546de9e76df57ae
-
-// app.configure = () => {
-//   app.use(express.static('public'));
-//   app.use(express.cookieParser());
-//   app.use(express.bodyParser());
-//   app.use(express.session({ secret: 'keyboard cat' }));
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-//   app.use(app.router);
-// };
-
-
-// app.post('/login',
-//   passport.authenticate('local'),
-//   function(req, res) {
-//     // If this function gets called, authentication was successful.
-//     // `req.user` contains the authenticated user.
-//     res.redirect('/users/' + req.user.username);
-//   });
-
-//   app.post('/login',
-//   passport.authenticate('local', { successRedirect: '/',
-//                                    failureRedirect: '/login' }));
-
-
-
